02a169cc0d292715c28c5aea46d82809097fa3c1
Vinicius Scandura <83542527+ScanVM@users.noreply.github.com>
2025-07-01 07:05:08 +0000

    Criacao dos scripts de formatacao de codigo com prettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto para aprendizado do curso.dev, tendo como objetivo a implementação do https://www.tabnews.com.br
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
-function Home(){
-    return <h1>Imagine aqui uma menssagem que irá causar impacto em  você ! 🖤 </h1>
+function Home() {
+  return (
+    <h1>Imagine aqui uma menssagem que irá causar impacto em você ! 🖤 </h1>
+  );
 }
 
 export default Home;
